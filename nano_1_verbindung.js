c59f8f728637146cf5c2e69c7d8eb77ce28376ce
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-29 13:42:36 +0200

    comment nano_1

--- nano_1_verbindung.js
+++ nano_1_verbindung.js
@@ -6,3 +6,24 @@ const connection = nano('http://127.0.0.1:5984');
 const db = connection.db;
 
 console.log(db);
+
+// Output: Shows nano database scope object
+// ---------------------------------------
+/* {
+    create: [Function: createDb],
+    get: [Function: getDb],
+    destroy: [Function: destroyDb],
+    list: [Function: listDbs],
+    listAsStream: [Function: listDbsAsStream],
+    use: [Function: docModule],
+    scope: [Function: docModule],
+    compact: [Function: compactDb],
+    replicate: [Function: replicateDb],
+    replication: {
+      enable: [Function: enableReplication],
+      disable: [Function: disableReplication],
+      query: [Function: queryReplication]
+    },
+    changes: [Function: changesDb],
+    updates: [Function: updates]
+  } */
