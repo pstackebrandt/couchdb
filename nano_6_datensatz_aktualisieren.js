ce86ef71d5bc5fb67991f650c8925ac59a20e94f
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-29 14:08:41 +0200

    comment 6

--- nano_6_datensatz_aktualisieren.js
+++ nano_6_datensatz_aktualisieren.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Credentials für die Anmeldung in Couch
-const username = 'alfa', password = 'alfa';
+const username = 'admin', password = 'asy';
 const dbName = 'autos';
 
 const meinAuto = {
@@ -22,7 +22,7 @@ const init = () => {
     let myDB = db.use(dbName);
 
     // insert() dient auch dem Aktualisieren
-    // Dazu muss die _id UND die _rev mitgegeben werden
+    // Dazu muss die _id UND die _rev mitgegeben werden (Das wird hier jedoch wohl nicht gemacht.)
     // Es werden nur die Attribute gespeichert, die übergeben wurden. Auf diese Weise können Attribute entfernt werden
     myDB.insert(meinAuto).then(
         console.log
@@ -33,3 +33,13 @@ const init = () => {
 }
 
 init();
+
+// Vorher DB 'autos' anlegen!!
+// Output:
+/* {
+    ok: true,
+    id: '53df0649e94dc8f94c02e196d9006958',
+    rev: '1-9d44f4de9e81e673cd73b888fa88292c'
+  } */
+
+// Bei erneutem Aufruf wird die ID geändert, aber die rev bleibt gleich
