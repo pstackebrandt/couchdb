30a4b985af1a31296a19cdea4b52aad24ecf5b45
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-29 13:53:24 +0200

    comment and rename script 3

--- nano_3_db_anlegen_oder_auflisten.js
+++ nano_3_db_anlegen_oder_auflisten.js
@@ -1,13 +1,11 @@
 'use strict';
 
 // Credentials für die Anmeldung in Couch
-const username = 'alfa', password = 'alfa';
-const dbName = 'example2';
+const username = 'admin', password = 'asy';
+const dbName = 'testdb0';
 
 // Verbindung zu Couch herstellen
 const db = require('nano')(`http://${username}:${password}@127.0.0.1:5984`).db;
-
-
 
 const init = () => {
 
@@ -29,3 +27,5 @@ const init = () => {
 }
 
 init();
+
+// Output: DB "testdb0" existiert bereits
