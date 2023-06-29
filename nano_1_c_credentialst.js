3cf1a635f827fc0f78a2376a011ae2bfad796063
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-06-29 13:41:52 +0200

    comment 1, 1b, 1c

--- nano_1_c_credentialst.js
+++ nano_1_c_credentialst.js
@@ -1,9 +1,11 @@
 'use strict';
 
 // Credentials für die Anmeldung in Couch
-const username = 'alfa', password = 'alfa';
+const username = 'admin', password = 'asy';
 
 // Verbindung zu Couch herstellen
 const db = require('nano')(`http://${username}:${password}@127.0.0.1:5984`).db;
+
+// Output: Shows nano database scope object
 
 console.log(db);
--- nano_1b_kompakt.js
+++ nano_1b_kompakt.js
@@ -1,6 +1,9 @@
 'use strict';
 
-// Verbindung zu Couch herstellen
+// kompakt Verbindung zu Couch herstellen
+// without login
 const db = require('nano')('http://127.0.0.1:5984').db;
 
 console.log(db);
+
+// Output: Shows nano database scope object
